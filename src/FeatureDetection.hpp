f6a40bd26d30e6075d3f114299b5e8b561d36f91
Xiangyu Bi <xib008@ucsd.edu>
2018-03-27 17:55:51 -0700

    s

--- src/FeatureDetection.hpp
+++ src/FeatureDetection.hpp
@@ -20,7 +20,23 @@ public:
 };
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr FeatureDetection::readPLYFile(const char *plyname) {
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new )
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    if(pcl::io::loadPLYFile<pcl::PointXYZRGB>(plyname, *cloud) == -1)
+        throw("Cannot open ply file...");
+
+    std::cout << "Loaded "
+              << cloud->width * cloud->height
+              << " data points from test_pcd.pcd with the following fields: "
+              << std::endl;
+
+    for (size_t i = 0; i < cloud->points.size (); ++i)
+        std::cout << "    " << cloud->points[i].x
+                  << " "    << cloud->points[i].y
+                  << " "    << cloud->points[i].z << std::endl;
+
+    return cloud;
+
 }
 
 
